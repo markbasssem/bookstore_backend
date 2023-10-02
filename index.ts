7a29b6ac6c42a95296f4ab3270c285807b2fbaf1
markbasssem <mark.bassem@thirdwayv.com>
2023-10-02 15:02:40 +0300

    Revert "test"
    This reverts commit d0610deed4609b1876c2979abec7400c81ea49c7.

--- index.ts
+++ index.ts
@@ -17,9 +17,9 @@ app.use("/auth", auth);
 app.use("/users", users);
 
 mongoose
-  .connect("mongodb+srv://mark:mark@cluster0.2q3nfl2.mongodb.net/")
+  .connect("mongodb://localhost:27017/bookstore")
   .then(() => {
-    app.listen(3000, "0.0.0.0", () => console.log("App listening"));
+    app.listen(3000, () => console.log("App listening"));
   })
   .catch((err) => {
     console.log(err);
